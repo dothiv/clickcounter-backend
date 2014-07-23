36ff1dab892b942d23c64ccddb36f78d1543769a
Markus Tacker <m@coderbyheart.de>
2014-07-23 18:45:39 +0200

    fix(app): renamed crontab, remove unused route

--- main.py
+++ main.py
@@ -3,9 +3,8 @@
 import webapp2
 
 application = webapp2.WSGIApplication([
-    ('/', 'handlers.Index'),
     ('/redirects', 'redirects.List'),
     (r'/config/(.*)', 'handlers.Config'),
     (r'/c/?', 'handlers.Count'),
     ('/task/counter/persist', 'jobs.CounterPersist')
-], debug=True)
+])
